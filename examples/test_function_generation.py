1974b47cd9289931beb46686607681d1558a828d
David Stein <dstein@flatironinstitute.org>
2019-03-15 16:07:55 -0400

    modified example

--- examples/test_function_generation.py
+++ examples/test_function_generation.py
@@ -2,14 +2,14 @@ import numpy as np
 import fast_interp
 import time
 import numexpr as ne
-from scipy.special import struve
+from scipy.special import struve, y0
 
 n = 1000000
 
 # function to test evaluation of
 # in this case, nasty and singular at the origin
 def true_func(x):
-	return struve(-3, x)*np.exp(x/10.0)*np.sin(10*x)/(1 + 0.99*np.cos(x))*np.log(np.sqrt(x+1.0)+1.0)
+	return struve(0, x) - y0(x)
 approx_range = [1e-14, 10]
 
 # generate approximation
@@ -45,13 +45,13 @@ err5 = np.abs(fa5-ft)/reg
 err7 = np.abs(fa7-ft)/reg
 
 print('')
-print('Error (5):       {:0.1e}'.format(err5.max()))
-print('Error (7):       {:0.1e}'.format(err7.max()))
-print('True time:       {:0.1f}'.format(true_func_time*1000))
-print('Approx time (5): {:0.1f}'.format(approx_func5_time*1000))
-print('Approx time (7): {:0.1f}'.format(approx_func7_time*1000))
+print('Error (5):        {:0.1e}'.format(err5.max()))
+print('Error (7):        {:0.1e}'.format(err7.max()))
+print('True time:        {:0.1f}'.format(true_func_time*1000))
+print('Approx time (5):  {:0.1f}'.format(approx_func5_time*1000))
+print('Approx time (7):  {:0.1f}'.format(approx_func7_time*1000))
 
-n = 5000
+n = 10000
 x = np.random.rand(n)
 y = np.random.rand(n)
 r = np.empty([n, n], dtype=float)
@@ -69,5 +69,7 @@ out = ker(x, y, tau, r, G)
 st = time.time()
 out = ker(x, y, tau, r, G)
 kernel_time = time.time()-st
+
+print('Kernel Eval Time: {:0.1f}'.format(kernel_time*1000))
 
 
